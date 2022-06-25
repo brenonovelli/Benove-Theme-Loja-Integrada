829358c029e3a2e1ac59af20380ecab229a268d5
Breno Novelli <bnojpa@gmail.com>
2022-06-25 18:42:13 -0300

    update theme

--- src/scripts/benove_script.js
+++ src/scripts/benove_script.js
@@ -17,9 +17,18 @@
   const newIconPhone = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>`;
   iconPhone && iconPhone.insertAdjacentHTML("afterBegin", newIconPhone);
 
-  const iconCart = document.querySelector(".carrinho .icon-shopping-cart");
   const newIconCart = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>`;
+
+  const iconCart = document.querySelector(".carrinho .icon-shopping-cart");
   iconCart && iconCart.insertAdjacentHTML("afterBegin", newIconCart);
+
+  const iconButtonCart = document.querySelectorAll(
+    ".botao .icon-shopping-cart"
+  );
+  iconButtonCart &&
+    iconButtonCart.forEach((item) =>
+      item.insertAdjacentHTML("afterBegin", newIconCart)
+    );
 
   const iconHome = document.querySelector(".atalhos-mobile .icon-home");
   const newIconHome = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>`;
